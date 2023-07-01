545d656ba06e7fd0c3b4c11eb065e5fc54f51778
zulfuqar2 <anarzulfuqar2005@gmail.com>
2023-07-01 08:46:37 +0400

    update react 17 to 18 code is clear but i have one problem my code is not show is google

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
-ReactDOM.render(
+
+
+const root =ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <div>
+  {/* <App/> */}
   <Router>
     <Routes path="/"element={App} />
   </Router>,
-  document.getElementById('root')
-);
+    </div>
+
+)
