d70572379065b8edf6dcd4642b410a93262d55ce
zulfuqar2 <anarzulfuqar2005@gmail.com>
2023-07-02 09:27:30 +0400

    still writing code

--- src/componnets/NavBAr/Projects/Projects.jsx
+++ src/componnets/NavBAr/Projects/Projects.jsx
@@ -1,22 +1,62 @@
-import React from 'react'
-import './Projects.css'
+import React from "react";
+import "./Projects.css";
 function Projects() {
   return (
-    <div className='containers'>
-         <h1>My Projects</h1>
-         <div className='links'>
-
-         
-         <a href="https://spiffy-choux-1821a9.netlify.app" className='WheAp'>1</a>
-         <a href="https://moonlit-starlight-bdca63.netlify.app/" className='MoovieApp'>2</a>
-         <a href="https://fantastic-cactus-6277ba.netlify.app/"  className='Converter'>3</a>
-         <a href="https://graceful-malasada-06227f.netlify.app/" className='QuizApp'>4</a>
-         <a href="https://splendid-lolly-282395.netlify.app/" className='SignIn'>5</a>
-         <a href="https://velvety-valkyrie-46a0d7.netlify.app/" className='HtmlCss'>6</a>
-         </div>
-
+    <div className="box">
+      <h1 className="proj">Projects</h1>
+      <div className="links">
+        <ul>
+          <li>
+            {" "}
+            <a
+              href="https://loquacious-donut-c9583b.netlify.app"
+              className="WheAp"
+            ></a>
+          </li>
+          <li>
+            {" "}
+            <a
+              href="https://moonlit-starlight-bdca63.netlify.app/"
+              className="MoovieApp"
+            ></a>
+          </li>
+          <li>
+            {" "}
+            <a
+              href="https://fantastic-cactus-6277ba.netlify.app/"
+              className="Converter"
+            ></a>
+          </li>
+          <li>
+            {" "}
+            <a
+              href="https://graceful-malasada-06227f.netlify.app/"
+              className="QuizApp"
+            >
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a
+              href="https://splendid-lolly-282395.netlify.app/"
+              className="SignIn"
+            >
+              
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a
+              href="https://velvety-valkyrie-46a0d7.netlify.app/"
+              className="HtmlCss"
+            >
+              
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
--- src/componnets/NavBAr/SubMsg/SubText.jsx
+++ src/componnets/NavBAr/SubMsg/SubText.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './SubText.css'
 function SubText() {
   return (
-    <div>SubText</div>
+    <>
+    </>
   )
 }
 
